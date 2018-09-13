4bde731511b0af67c921b37ef660d9b117517ff7
Dennis Ushakov <dennis.ushakov@gmail.com>
2018-09-13 14:48:59 +0300

    2018.2 compatibility

--- src/main/kotlin/com/github/denofevil/importCost/ServiceProtocol.kt
+++ src/main/kotlin/com/github/denofevil/importCost/ServiceProtocol.kt
@@ -1,30 +1,18 @@
 package com.github.denofevil.importCost
 
-import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager
-import com.intellij.javascript.nodejs.interpreter.local.NodeJsLocalInterpreter
-import com.intellij.javascript.nodejs.interpreter.local.NodeJsLocalInterpreterManager
 import com.intellij.lang.javascript.service.JSLanguageServiceUtil
 import com.intellij.lang.javascript.service.protocol.JSLanguageServiceInitialState
 import com.intellij.lang.javascript.service.protocol.JSLanguageServiceNodeStdProtocolBase
+import com.intellij.lang.javascript.service.protocol.LocalFilePath
 import com.intellij.openapi.project.Project
 import com.intellij.util.Consumer
 
-class ServiceProtocol(private val project: Project, readyConsumer: Consumer<*>) : JSLanguageServiceNodeStdProtocolBase(project,
-        readyConsumer) {
-    override fun getNodeInterpreter(): String? {
-        val node = NodeJsInterpreterManager.getInstance(project).interpreter ?: return null
-        var localInterpreter = NodeJsLocalInterpreter.tryCast(node)
-        if (localInterpreter == null || !localInterpreter.isValid) {
-            localInterpreter = NodeJsLocalInterpreterManager.getInstance().detectMostRelevant()
-        }
-        return localInterpreter?.interpreterSystemDependentPath
-    }
-
+class ServiceProtocol(project: Project, readyConsumer: Consumer<*>) : JSLanguageServiceNodeStdProtocolBase(project, readyConsumer) {
     override fun createState(): JSLanguageServiceInitialState {
         val result = JSLanguageServiceInitialState()
         result.pluginName = "import-cost"
         val file = JSLanguageServiceUtil.getPluginDirectory(this.javaClass, "lib/index.js")
-        result.pluginPath = file.absolutePath
+        result.pluginPath = LocalFilePath.create(file.absolutePath)
         return result
     }
 
